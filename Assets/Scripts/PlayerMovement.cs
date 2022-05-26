14334e4af0b61b13bc30cabffae074aa99ca23eb
Dawson Naylor <Dawson@Davids-iMac.local>
2022-05-26 06:18:50 -0500

    Release project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	
+	public Rigidbody rb;
+
+	public float forwardForce = 2000f;	
+	public float sidewaysForce = 500f;  
+
+
+	void FixedUpdate ()
+	{
+		
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+		if (Input.GetKey("d"))	
+		{
+			
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (Input.GetKey("a"))  
+		{
+			
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+				if (Input.GetKey(KeyCode.RightArrow))  
+		{
+			
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+				if (Input.GetKey(KeyCode.LeftArrow))  
+		{
+			
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (rb.position.y < -1.5f)
+		{
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+}
